153b88a1b8b8673f906a5ecea0edff79694cdb3d
anandiselect <anand.potluri@iselect.com.au>
2021-09-28 06:15:25 +0530

    Removed unnecessary console.log statement

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,3 @@
-console.log("Client side javascript loaded...");
-
-
-
 const weatherForm = document.querySelector('form');
 const searchEle = document.querySelector('input');
 const errorEle = document.querySelector('.error');
